aba446e5c7b2ed0fc51dd5d6c7ed7874372aaf48
SYED MOHD ASKARI <82087982+syed-mohd-askari@users.noreply.github.com>
2024-08-30 02:26:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-
-const API_KEY = secrets.API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = "api_key=0ea5c8dae2bf865c3e995a979bf6e03e";
 const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&include_adult=false&" + API_KEY;
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
 const SEARCH_URL = BASE_URL + "/search/movie?query=";
